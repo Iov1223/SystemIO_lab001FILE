26e3de58ee4b9d589cb08bf5104df1b67252ad29
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-07-08 18:50:17 +0500

    Добавьте файлы проекта.

---
+++ SystemIO_lab001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SystemIO_lab001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _myPath = Path.GetTempPath();
+            Console.WriteLine("Путь к папке \"TEMP\" = {0}", _myPath);
+            _myPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Console.WriteLine("Путь к папке \"Мои документы\" = {0}", _myPath);
+            _myPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Console.WriteLine("Путь к папке \"Профиль текущего пользователя\" = {0}", _myPath);
+        }
+    }
+}
